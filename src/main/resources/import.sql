5e0fb8a5ec38f313a34b3d92013e84436195e789
Kiko Previero <kikoprevi@hotmail.com>
2019-09-23 20:35:10 -0300

    Step 4

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into post (id, title, body) values (1005,'test', 'test');
+insert into post (id, title, body) values (1006,'test', 'test');
+insert into post (id, title, body) values (1007,'test', 'test');
+insert into post (id, title, body) values (1008,'test', 'test');
+insert into post (id, title, body) values (1009,'test', 'test');
